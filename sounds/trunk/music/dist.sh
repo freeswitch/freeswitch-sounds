97f53a92c2299f9c9ac7f78b5183595939ae765c
brian <brian@d0543943-73ff-0310-b7d9-9358b9ac24b2>
2007-12-11 22:56:04 +0000

    add hold music
    git-svn-id: file:///tmp/freeswitch-sounds.svn@31 d0543943-73ff-0310-b7d9-9358b9ac24b2

---
+++ sounds/trunk/music/dist.sh
@@ -0,0 +1,24 @@
+base_dir="48000"
+rates="32000 16000 8000"
+version="1.0.pre3"
+voice="music"
+voice_dir="music"
+
+for rate in $rates; do \
+  test -d $rate || mkdir $rate; \
+done
+
+for filename in `ls $base_dir`; do \
+  for rate in $rates; do \
+    echo sox $base_dir/$filename -r $rate -c 1 $rate/$filename; \
+    sox $base_dir/$filename -r $rate -c 1 $rate/$filename; \
+  done ; \
+done
+
+
+cd ..
+for rate in $rates; do \
+  echo tar -cvzf freeswitch-sounds-$voice-$rate-$version.tar.gz $voice_dir/$rate; \
+  tar -cvzf freeswitch-sounds-$voice-$rate-$version.tar.gz $voice_dir/$rate; \
+done
+cd -
