aa062054c5fcc9b04fc9b24e6faf3fb582f16b40
mikej <mikej@d0543943-73ff-0310-b7d9-9358b9ac24b2>
2007-12-12 18:03:13 +0000

    new dir structure
    git-svn-id: file:///tmp/freeswitch-sounds.svn@32 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- sounds/trunk/en/us/callie/dist.sh
+++ sounds/trunk/en/us/callie/dist.sh
@@ -3,29 +3,23 @@ rates="32000 16000 8000"
 version="1.0.pre3"
 voice="en-us-callie"
 voice_dir="en/us/callie"
+tar_path="../../.."
+tmp_dir="tmp"
+CWD=`pwd`
 
 for rate in $rates; do \
-  test -d $rate || mkdir $rate; \
-done
-
-for dir in `ls $base_dir`; do \
-  for rate in $rates; do \
-    test -d $rate/$dir || mkdir $rate/$dir; \
-  done ; \
-done
-
-for dir in `ls $base_dir`; do \
-  for filename in `ls $base_dir/$dir`; do \
-    for rate in $rates; do \
-      echo sox $base_dir/$dir/$filename -r $rate -c 1 $rate/$dir/$filename; \
-      sox $base_dir/$dir/$filename -r $rate -c 1 $rate/$dir/$filename; \
+  mkdir -p $tmp_dir/$voice_dir
+  for dir in `ls $base_dir`; do \
+    test -d $tmp_dir/$voice_dir/$dir/$rate || mkdir -p $tmp_dir/$voice_dir/$dir/$rate; \
+    for filename in `ls $base_dir/$dir`; do \
+      echo sox $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
+      sox $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
     done ; \
   done ; \
+  cd $tmp_dir
+  echo tar -cvzf $CWD/$tar_path/freeswitch-sounds-$voice-$rate-$version.tar.gz *; \
+  tar -cvzf $CWD/$tar_path/freeswitch-sounds-$voice-$rate-$version.tar.gz *; \
+  cd -
+  rm -rf $tmp_dir
 done
 
-cd ../../..
-for rate in $rates; do \
-  echo tar -cvzf freeswitch-sounds-$voice-$rate-$version.tar.gz $voice_dir/$rate; \
-  tar -cvzf freeswitch-sounds-$voice-$rate-$version.tar.gz $voice_dir/$rate; \
-done
-cd -
