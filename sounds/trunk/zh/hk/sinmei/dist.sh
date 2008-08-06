12264739a56f827f05392366d189094586626e3d
coppice <coppice@d0543943-73ff-0310-b7d9-9358b9ac24b2>
2008-08-06 12:31:34 +0000

    A poor (rather slow) and incomplete set of HK Cantonese and Mandarin prompts to at least start getting the tree populated
    git-svn-id: file:///tmp/freeswitch-sounds.svn@57 d0543943-73ff-0310-b7d9-9358b9ac24b2

---
+++ sounds/trunk/zh/hk/sinmei/dist.sh
@@ -0,0 +1,25 @@
+base_dir="48000"
+rates="32000 16000 8000"
+version="0.0.1"
+voice="zh-hk-sinmei"
+voice_dir="zh/hk/sinmei"
+tar_path="../../.."
+tmp_dir="tmp"
+CWD=`pwd`
+
+for rate in $rates; do \
+  mkdir -p $tmp_dir/$voice_dir
+  for dir in `ls $base_dir`; do \
+    test -d $tmp_dir/$voice_dir/$dir/$rate || mkdir -p $tmp_dir/$voice_dir/$dir/$rate; \
+    for filename in `ls $base_dir/$dir`; do \
+      echo sox -v 0.2 $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
+      sox -v 0.2 $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
+    done ; \
+  done ; \
+  cd $tmp_dir
+  echo tar -cvzf $CWD/$tar_path/freeswitch-sounds-$voice-$rate-$version.tar.gz *; \
+  tar -cvzf $CWD/$tar_path/freeswitch-sounds-$voice-$rate-$version.tar.gz *; \
+  cd -
+  rm -rf $tmp_dir
+done
+
