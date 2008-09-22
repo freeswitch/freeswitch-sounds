54131a7c1cdaeb46d7af8b6c21f4e28a80d27036
mikej <mikej@d0543943-73ff-0310-b7d9-9358b9ac24b2>
2008-09-22 17:24:06 +0000

    add voicemail/vm-too-small.wav and bump to 1.0.6
    git-svn-id: file:///tmp/freeswitch-sounds.svn@68 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- sounds/trunk/en/us/callie/dist.sh
+++ sounds/trunk/en/us/callie/dist.sh
@@ -1,6 +1,6 @@
 base_dir="48000"
 rates="32000 16000 8000"
-version="1.0.5"
+version="1.0.6"
 voice="en-us-callie"
 voice_dir="en/us/callie"
 tar_path="../../.."
--- sounds/trunk/music/dist.sh
+++ sounds/trunk/music/dist.sh
@@ -1,6 +1,6 @@
 base_dir="48000"
 rates="32000 16000 8000"
-version="1.0.5"
+version="1.0.6"
 voice="music"
 voice_dir="music"
 
