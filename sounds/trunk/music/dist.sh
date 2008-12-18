b3e568e7c4987ed620bcdb52802c20ea0d6337a8
brian <brian@d0543943-73ff-0310-b7d9-9358b9ac24b2>
2008-12-18 16:00:33 +0000

    more sounds
    git-svn-id: file:///tmp/freeswitch-sounds.svn@71 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- sounds/trunk/music/dist.sh
+++ sounds/trunk/music/dist.sh
@@ -1,6 +1,6 @@
 base_dir="48000"
 rates="32000 16000 8000"
-version="1.0.6"
+version="1.0.7"
 voice="music"
 voice_dir="music"
 
@@ -18,7 +18,9 @@ done
 
 cd ..
 for rate in $rates; do \
-  echo tar -cvzf freeswitch-sounds-$voice-$rate-$version.tar.gz $voice_dir/$rate; \
-  tar -cvzf freeswitch-sounds-$voice-$rate-$version.tar.gz $voice_dir/$rate; \
+  echo tar -cvzf freeswitch-sounds-$voice-$rate-$version.tar.gz $voice_dir/$rate--exclude .svn;  \
+  tar -cvzf freeswitch-sounds-$voice-$rate-$version.tar.gz $voice_dir/$rate --exclude .svn; \
 done
+  tar -cvzf freeswitch-sounds-$voice-48000-$version.tar.gz $voice_dir/48000 --exclude .svn; \
+
 cd -
