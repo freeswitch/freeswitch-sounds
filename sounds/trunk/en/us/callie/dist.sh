3bf3ac4bdba64da477a6082cee881ad0ccc7bbc1
brian <brian@d0543943-73ff-0310-b7d9-9358b9ac24b2>
2009-09-11 02:07:46 +0000

    update to 1.0.12 sounds
    git-svn-id: file:///tmp/freeswitch-sounds.svn@92 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- sounds/trunk/en/us/callie/dist.sh
+++ sounds/trunk/en/us/callie/dist.sh
@@ -1,6 +1,6 @@
 base_dir="48000"
 rates="48000 32000 16000 8000"
-version="1.0.11"
+version="1.0.12"
 voice="en-us-callie"
 voice_dir="en/us/callie"
 tar_path="../../.."
--- sounds/trunk/ru/RU/elena/dist.sh
+++ sounds/trunk/ru/RU/elena/dist.sh
@@ -1,6 +1,6 @@
 base_dir="44000"
 rates="48000 32000 16000 8000"
-version="1.0.11"
+version="1.0.12"
 voice="ru-RU-elena"
 voice_dir="ru/RU/elena"
 tar_path="../../.."
