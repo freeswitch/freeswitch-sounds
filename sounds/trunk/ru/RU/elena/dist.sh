351b11a425f321fd367f6ad30c87dfe45b445a3f
buklov <buklov@d0543943-73ff-0310-b7d9-9358b9ac24b2>
2009-11-06 11:35:26 +0000

    change num to 13, vm-marked_urgent --> vm-marked-urgent
    git-svn-id: file:///tmp/freeswitch-sounds.svn@94 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- sounds/trunk/ru/RU/elena/dist.sh
+++ sounds/trunk/ru/RU/elena/dist.sh
@@ -1,6 +1,6 @@
 base_dir="44000"
 rates="48000 32000 16000 8000"
-version="1.0.12"
+version="1.0.13"
 voice="ru-RU-elena"
 voice_dir="ru/RU/elena"
 tar_path="../../.."
@@ -12,8 +12,8 @@ for rate in $rates; do \
   for dir in `ls $base_dir`; do \
     test -d $tmp_dir/$voice_dir/$dir/$rate || mkdir -p $tmp_dir/$voice_dir/$dir/$rate; \
     for filename in `ls $base_dir/$dir`; do \
-      echo sox -v 0.2 $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
-      sox -v 0.2 $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
+      echo sox $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
+      sox $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
     done ; \
   done ; \
   cd $tmp_dir
