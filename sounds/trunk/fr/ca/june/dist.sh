90ebaa83dfc4a9852ac9e9c70f1343b76752f402
Michael S Collins <msc@freeswitch.org>
2011-05-05 15:13:34 -0700

    Add vm-messages.wav to fr; add dist.sh to fr

---
+++ sounds/trunk/fr/ca/june/dist.sh
@@ -0,0 +1,31 @@
+base_dir="48000"
+rates="48000 32000 16000 8000"
+version="1.0.13"
+voice="fr-ca-june"
+voice_dir="fr/ca/june"
+tar_path="../../.."
+tmp_dir="tmp"
+CWD=`pwd`
+
+for rate in $rates; do \
+  mkdir -p $tmp_dir/$voice_dir
+  for dir in `ls $base_dir`; do \
+    test -d $tmp_dir/$voice_dir/$dir/$rate || mkdir -p $tmp_dir/$voice_dir/$dir/$rate; \
+    for filename in `ls $base_dir/$dir`; do \
+      echo sox -v 0.2 $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
+      sox -v 0.2 $base_dir/$dir/$filename -r $rate -c 1 $tmp_dir/$voice_dir/$dir/$rate/$filename; \
+    done ; \
+  done ; \
+  cd $tmp_dir
+  echo tar -cvzf $CWD/$tar_path/freeswitch-sounds-$voice-$rate-$version.tar.gz *; \
+  tar -cvzf $CWD/$tar_path/freeswitch-sounds-$voice-$rate-$version.tar.gz *; \
+  cd -
+  rm -rf $tmp_dir
+done
+
+cd $tar_path
+for rate in $rates; do \
+  openssl dgst -sha1 freeswitch-sounds-$voice-$rate-$version.tar.gz > freeswitch-sounds-$voice-$rate-$version.tar.gz.sha1 ; \
+  openssl dgst -md5 freeswitch-sounds-$voice-$rate-$version.tar.gz > freeswitch-sounds-$voice-$rate-$version.tar.gz.md5 ; \
+done
+cd $CWD
